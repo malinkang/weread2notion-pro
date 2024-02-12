ea8e52c3525001153e13c143bafee21362a25c1f
lengruohanqiu2 <159115519+lengruohanqiu2@users.noreply.github.com>
2024-02-12 09:27:37 +0800

    引用颜色设置

--- scripts/utils.py
+++ scripts/utils.py
@@ -141,6 +141,7 @@ def get_quote(content):
 def get_callout(content, style, colorStyle, reviewId):
     # 根据不同的划线样式设置不同的emoji 直线type=0 背景颜色是1 波浪线是2
     emoji = "📝"
+    color = "default"
     if style == 0:
         emoji = "💡"
     elif style == 1:
@@ -148,7 +149,7 @@ def get_callout(content, style, colorStyle, reviewId):
     # 如果reviewId不是空说明是笔记
     if reviewId != None:
         emoji = "✍️"
-    color = "default"
+        color = "orange"
     # 根据划线颜色设置文字的颜色
     # if colorStyle == 1:
     #     color = "red"
