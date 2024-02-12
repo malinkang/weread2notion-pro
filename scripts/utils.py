46b7c3e0693312d1e4fcf304825d4bd0045b03d4
lengruohanqiu2 <159115519+lengruohanqiu2@users.noreply.github.com>
2024-02-12 09:28:41 +0800

    引用颜色还原

--- scripts/utils.py
+++ scripts/utils.py
@@ -133,7 +133,7 @@ def get_quote(content):
                     "text": {"content": content[:MAX_LENGTH]},
                 }
             ],
-            "color": "orange",
+            "color": "default",
         },
     }
 
