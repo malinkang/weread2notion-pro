4e943de2f21c3139fd365dfddb4440109bab2ab6
malinkang <malinkang@adsconflux.com>
2024-04-30 08:56:47 +0800

    fix heatmap

--- scripts/read_time.py
+++ scripts/read_time.py
@@ -80,7 +80,7 @@ def get_file():
         print("OUT_FOLDER does not exist.")
         return None
 
-
+HEATMAP_GUIDE = "https://mp.weixin.qq.com/s?__biz=MzI1OTcxOTI4NA==&mid=2247484145&idx=1&sn=81752852420b9153fc292b7873217651&chksm=ea75ebeadd0262fc65df100370d3f983ba2e52e2fcde2deb1ed49343fbb10645a77570656728&token=157143379&lang=zh_CN#rd"
 if __name__ == "__main__":
     notion_helper = NotionHelper()
     weread_api = WeReadApi()
@@ -93,9 +93,9 @@ if __name__ == "__main__":
                 block_id=notion_helper.heatmap_block_id, url=heatmap_url
             )
         else:
-            response = notion_helper.append_blocks(
-                block_id=notion_helper.page_id, children=[get_embed(heatmap_url)]
-            )
+            print(f"更新热力图失败，没有添加热力图占位。具体参考：{HEATMAP_GUIDE}")
+    else:
+        print(f"更新热力图失败，没有生成热力图。具体参考：{HEATMAP_GUIDE}")
     api_data = weread_api.get_api_data()
     readTimes = {int(key): value for key, value in api_data.get("readTimes").items()}
     now = pendulum.now("Asia/Shanghai").start_of("day")
